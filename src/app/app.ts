4fc335bbb65e3aa73873465d15543d877b68a1d7
Tetiana Sobolieva <tetiana.sobolieva.s@gmail.com>
2026-09-10 19:23:01 +0300

    added styles, todos html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+
+const todos = [
+  { id: 1, title: 'HTML + CSS', completed: true },
+  { id: 2, title: 'JS', completed: true },
+  { id: 3, title: 'React', completed: true },
+  { id: 4, title: 'Angular', completed: true },
+  { id: 5, title: 'Vue', completed: false },
+];
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-todo-app');
+  editing = false;
+  todos = todos;
 }
